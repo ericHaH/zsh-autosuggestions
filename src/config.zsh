8400edc3bc55ebb255bb079eae97ce1e58e3c352
Eric Freese <ericdfreese@gmail.com>
2018-11-23 10:18:15 -0700

    Implement completion suggestion strategy (#111)
    Based on https://github.com/Valodim/zsh-capture-completion
    
    `zpty -r` with a pattern seems to have some funky behavior on older
    versions, giving unpredictable results
    
    Don't use `-s` option to `zmodload`. It is not available in zsh versions
    older than 5.3
    
    If running in sync mode and a completion takes a long time, the user can
    ^C out of it. We need to use `always` in the strategy function or the
    pty will not be destroyed in this case and the next time we go to create
    it, it will fail, making the shell unusable.
    
    User can have many different completion styles set that will modify what
    they've already typed. These styles will result in suggestions that
    don't match what the user has already typed. We try our best to unset
    some of the more problematic ones, but add some code to fetch to
    invalidate suggestions that don't match what the user's already typed.

--- src/config.zsh
+++ src/config.zsh
@@ -89,3 +89,7 @@ typeset -g ZSH_AUTOSUGGEST_ORIGINAL_WIDGET_PREFIX=autosuggest-orig-
 # Max size of buffer to trigger autosuggestion. Leave null for no upper bound.
 (( ! ${+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE} )) &&
 typeset -g ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=
+
+# Pty name for capturing completions for completion suggestion strategy
+(( ! ${+ZSH_AUTOSUGGEST_COMPLETIONS_PTY_NAME} )) &&
+typeset -g ZSH_AUTOSUGGEST_COMPLETIONS_PTY_NAME=zsh_autosuggest_completion_pty
